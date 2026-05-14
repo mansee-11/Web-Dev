3544dea0b2c0f8b1f0fd8b732dee96ca5878c89e
mansee-11 <silochanaasati30@gmail.com>
2026-05-14 09:57:56 +0530

    tic tac toe

--- JS/lect06/tictac.js
+++ JS/lect06/tictac.js
@@ -4,7 +4,7 @@ let msgBox=document.querySelector("#msg")
 let reset=document.querySelector("#reset")
 let win=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 reset.addEventListener("click",()=>{
-    draw.innerText=""
+    msgBox.innerText=""
     for(let a of box)
     {
         a.innerText=""
