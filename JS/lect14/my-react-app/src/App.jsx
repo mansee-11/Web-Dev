232ad00c9a64b42bca7aae4ccf05db5c37cf68de
mansee-11 <silochanaasati30@gmail.com>
2026-05-26 06:02:56 +0530

    react basics

---
+++ JS/lect14/my-react-app/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState } from 'react'
+let count=0
+// function fun1(){     by default funcional components are static 
+//   count++            and object-classes component is dynamic 
+//   console.log(count)      
+// }
+function App() {
+  let [count,SetCount] =useState(0)   //we use useState to make function dynamic
+  function fun1(){
+    SetCount(++count)
+  }
+
+  function fun2(){
+    SetCount(--count)
+  }
+
+  let [color,setColor] = useState("gold")
+  function fun3(){
+    setColor("pink")
+  }
+  return (<div style={{backgroundColor:color,width:"200px",height:"200px"}}>
+    {/* <h1>Mansee</h1>
+    <h2>hello</h2> */}
+    <h2>{count}</h2>
+    <button onClick={fun1}>Addd</button>
+    <button onClick={fun2}>sub</button>
+    <button onClick={fun3}>color</button>
+  </div>)
+}
+
+export default App
