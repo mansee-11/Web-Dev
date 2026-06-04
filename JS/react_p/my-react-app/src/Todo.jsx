82230ccd094cedf2049c68aa347b3c89d4686eea
mansee-11 <silochanaasati30@gmail.com>
2026-06-04 11:34:34 +0530

    todo list

--- JS/react_p/my-react-app/src/Todo.jsx
+++ JS/react_p/my-react-app/src/Todo.jsx
@@ -1,7 +1,70 @@
+// import React, { useEffect, useState } from 'react'
+
+// const Todo = () => {
+//     const [task,setTask] = useState("");
+//     const[todos,setTodos] = useState(()=>{
+//         let data =localStorage.getItem("key")
+//         if(data)
+//         {
+//             return JSON.parse(data)
+//         }
+//         return []
+//     });
+
+//     useEffect(()=>{
+//         localStorage.setItem("key",JSON.stringify(todos))
+//     },[todos])
+//     function fun1(){
+//         setTodos([...todos,task])
+//         console.log(todos)
+//     }
+//     function fun2(id){
+//         let UpdatedData = todos.filter((a,b)=>{
+//             return b!=id
+//         })
+//         setTodos(UpdatedData)
+//     }
+//     function fun3(id)
+//     {
+//         let data = prompt("enter new task")
+//         let UpdatedData =[...todos];
+//         UpdatedData[id]=data;
+//         setTodos(UpdatedData)
+//     }
+//   return (
+//     <div id='parent'>
+//         <h1>Todo List</h1>
+//         <div id='card'>
+//             <input type='text' name='task' value={task} onChange={(e)=>setTask(e.target.value)} placeholder='Enter a Task'/>
+//             <button onClick={fun1}>Add</button>
+//         </div>
+//         <div id='tasks'>
+//             {
+//                 todos.map((a,b)=>{
+//                     return(
+//                     <div id='task'>
+//                         <span>{a}</span>
+//                         <button onClick={()=>fun3(b)}>Edit</button>
+//                         <button onClick={()=>fun2(b)}>Delete</button>
+//                     </div>)
+
+//                 })
+//             }
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Todo
+
+//second option 
 import React, { useEffect, useState } from 'react'
 
 const Todo = () => {
     const [task,setTask] = useState("");
+
+    const [index,setIndex] = useState(null)
+
     const[todos,setTodos] = useState(()=>{
         let data =localStorage.getItem("key")
         if(data)
@@ -14,9 +77,23 @@ const Todo = () => {
     useEffect(()=>{
         localStorage.setItem("key",JSON.stringify(todos))
     },[todos])
+
     function fun1(){
-        setTodos([...todos,task])
-        console.log(todos)
+        if(task.trim()=="")
+        {
+            return;
+        }
+        if(index!==null){
+            let UpdatedData =[...todos];
+            UpdatedData[index]=task;
+            setTodos(UpdatedData)
+            setTask("")
+            setIndex(null)
+        }
+        else{
+            setTodos([...todos,task])
+            setTask("")
+        }
     }
     function fun2(id){
         let UpdatedData = todos.filter((a,b)=>{
@@ -25,21 +102,16 @@ const Todo = () => {
         setTodos(UpdatedData)
     }
     function fun3(id)
-    {
-        let data = prompt("enter new task")
-        let UpdatedData =todos;
-        UpdatedData[id]=data;
-
-        setTodos(UpdatedData)
-        localStorage.setItem("key",JSON.stringify(todos))
-        console.log(todos)
+    {   
+        setIndex(id)
+        setTask(todos[id])
     }
   return (
     <div id='parent'>
         <h1>Todo List</h1>
         <div id='card'>
             <input type='text' name='task' value={task} onChange={(e)=>setTask(e.target.value)} placeholder='Enter a Task'/>
-            <button onClick={fun1}>Add</button>
+            <button onClick={fun1}>{index!==null?"Update":"Add"}</button>
         </div>
         <div id='tasks'>
             {
