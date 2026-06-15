ad769976744e77789091ab6c4d2fe8181661d30d
mansee-11 <silochanaasati30@gmail.com>
2026-06-15 09:50:45 +0530

    task

---
+++ JS/react_p/my-react-app/src/Task.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Task = () => {
+  return (
+    <div id='parent'>
+        <div id='head'>
+            <h1>useContext Demo</h1>
+            <p>HII THIS IS JUST A SIMPLE TASK</p>
+        </div>
+        <div  id='body'>
+            <p>
+                Hii I am trying my best to be get
+                internships and placement but still
+                it is not feeling enough I don't what 
+                to do how to manage this all resting 
+                feels like a crime....
+            </p>
+        </div>
+        <button>Switch Mode</button>
+    </div>
+  )
+}
+
+export default Task
